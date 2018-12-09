dc9e681ab3cb897926a75babae49c810fc3e9ec7
cherylac <38063622+cherylac@users.noreply.github.com>
2018-12-09 13:29:39 -0800

    incremental adds

---
+++ src/components/Home.vue
@@ -0,0 +1,13 @@
+<template>
+   <div>
+       <h2>How is the air today?</h2>
+       <form v-on:submit.prevent="getCities">
+           <p>Enter your city and state: <input type="text" v-model="query" placeholder="ex: Seattle, WA"> <button type="submit">See the AQ</button></p>
+        </form>
+        <ul class="cities" v-if="results && results.list.length > 0">
+            <li v-for="(city, index) in results.list" v-bind:key="index">
+                <h2>{{ city.name }}, {{ city.sys.country }}</h2>
+                <p><router-link v-bind:to="{ name; 'CurrentAir', params: {cityId: city.id } }"</p>
+        </li>
+     </ul>
+    </div>
